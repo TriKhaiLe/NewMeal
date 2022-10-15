da8bec5c7ef288a70bcad2f7c7f67a794414f1ee
Le Khai Tri <lekhaitri.2003@gmail.com>
2022-10-15 13:43:41 +0700

    completed steps before Grid

--- Project/Project/LoginWindow.xaml.cs
+++ Project/Project/LoginWindow.xaml.cs
@@ -23,5 +23,13 @@ namespace Project
         {
             InitializeComponent();
         }
+
+        private void Window_MouseDown(object sender, MouseButtonEventArgs e)
+        {
+            if(e.LeftButton == MouseButtonState.Pressed)
+            {
+                DragMove();
+            }
+        }
     }
 }
