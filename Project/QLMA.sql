95bedfbc9d63c313bd7e8af3e598c241191e01c2
HuynhDangQuang <113320907+HuynhDangQuang@users.noreply.github.com>
2022-12-10 19:59:21 +0700

    Update QLMA.sql

--- Project/QLMA.sql
+++ Project/QLMA.sql
@@ -111,6 +111,18 @@ insert into Exercise values('14','Tennis','\Assets\Exercises\tennis.jpg','0.2')
 -------------Food---------------
 
 insert into Food values 
+('F101','Hủ tíu bò kho','538','Sơ chế rau củ-Sơ chế thịt bò-Nấu nước dùng bò kho','Nạm bò, hủ tiếu, cà rốt, cây sả, giá sống, hành tây, ngò gai, húng quế, rau răm, gia vị bò kho, màu hạt điều, hành tím, tỏi băm','Món ăn nước phổ biến','\Assets\Food\F101.jpg'),
+('F102','Hủ tíu Nam vang','400','Nấu nước lèo-Làm nước sốt hủ tiếu-Xào thịt heo băm','Lòng heo, khô mực, trứng cút, thịt băm, thịt ba chỉ, xương ống heo, gan heo, hành tím, gừng, củ cải, cần tây, tỏi, nước mắm, gia vị','Món ăn nước phổ biến ở Nam Bộ','\Assets\Food\F102.jpg'),
+('F103','Mì quảng','541','Sơ chế nguyên liệu-Nấu nước dùng và ướp gia vị-Làm nhân','Sợi mì Quảng, nhân tùy chọn (thịt, tôm, gà,...), rau sống, đậu phộng, bánh tráng','Đặc sản nổi tiếng miền Trung','\Assets\Food\F103.jpg'),
+('F104','Mì thịt heo','415','Sơ chế nguyên liệu-Bắc nồi nước-Cho xương và thịt lợn cùng cần tây, gừng vào nồi, nêm gia vị rồi hầm khoảng 30p-Chần mì rồi cho vào tô-Chan nước dùng, xếp thịt heo lên trên','Xương heo, thịt heo, cần tây, gừng, hành khô, hành lá, mì tươi, giá đỗ','Món ăn nước','\Assets\Food\F104.jpg'),
+('F105','Mì vịt tiềm','776','Sơ chế thịt vịt-Nấu nước dùng-Hầm thịt vịt trong nước dùng-Sơ chế mì','Mì vắt, đùi vịt, rượu trắng, gừng, tỏi băm, xì dầu, nước tương, tiêu, hoa hồi, thanh quế, trần bì, hành tím, táo tàu, bạch quả, củ sen, nấm hương, dầu mè, hạt nêm, đường phèn, muối','Món ăn ngon nổi tiếng của người Hoa','\Assets\Food\F105.jpg'),
+('F106','Miến gà','635','Sơ chế nguyên liệu-Ướp thịt gà-Nấu nước dùng','Gà, lòng gà, miến, hành tây, nấm mèo, hành lá, ngò rí, rau răm, hành tím, gia vị','Món ăn nước phổ biến','\Assets\Food\F106.jpg'),
+('F107','Phở bò chín','456','Sơ chế nguyên liệu-Làm nước dùng','Phở, thịt bò, gầu bò, xương ống bò, củ cải trắng, hành tây, hành tím, gừng, trái thảo quả, thanh quế nhỏ, hoa hồi, hành lá, giá đỗ, rau thơm, gia vị','Món ăn nước phổ biến','\Assets\Food\F107.jpg'),
+('F108','Phở bò tái','431','Sơ chế nguyên liệu-Làm nước dùng-Trụng phở và trần sơ thịt bò rồi vớt ra tô-Chan nước dùng','Phở, thịt bò, xương bò, hành tây, hành tím, gừng, hành hoa, rau mùi, gói gia vị nấu phở bò, gia vị khác','Món ăn nước phổ biến','\Assets\Food\F108.jpg'),
+('F109','Phở bò viên','431','Sơ chế bò-Sơ chế nguyên liệu-Ninh nước dùng-Cắt thịt bò-Trụng phở','Phở, xương bò, thịt bò, gân bò, nạm bò, gầu bò, bò viên, hoa hồi, thảo quả, quế, gừng, hành tây, hành tím, rau ăn kèm, gia vị','Món ăn nước phổ biến','\Assets\Food\F109.jpg'),
+('F110','Phở gà','483','Sơ chế nguyên liệu-Luộc gà và nấu nước dùng-Trụng phở','Phở, gà, giá đỗ, hành tây, sả, gừng, hành lá, ngò, ớt, chanh, gia vị','Món ăn nước phổ biến','\Assets\Food\F110.jpg');
+
+insert into Food values 
 ('F301','Canh bắp cải','37','Nấu sôi nước\nCho rau đã cắt vào nồi\nChờ rau chín tắt bếp rồi nêm gia vị','Bắp cải','Một món canh đơn giản','\Assets\Food\F301.jpg'),
 ('F302','Canh bầu','30', 'Nấu sôi nước\nCho bầu đã cắt lát vào nồi\nChờ bầu chín tắt bếp rồi nêm gia vị','Bầu','Một món canh đơn giản','\Assets\Food\F302.jpg'),
 ('F303','Canh bí đao','29', 'Nấu sôi nước\nCho bí đao đã cắt lát vào nồi\nChờ bí đao chín tắt bếp rồi nêm gia vị','Bí đao','Một món canh đơn giản','\Food\F303.jpg'),
