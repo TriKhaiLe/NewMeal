2c0115633d66c9b52d8d9cbf55aa3093acb26449
Huynh Dang Quang <quangptnk@gmail.com>
2022-12-25 15:17:35 +0700

    reup Account Page

---
+++ Project/Project - packup/MainWindow.xaml.cs
@@ -0,0 +1,99 @@
+﻿using Project.Model;
+using Project.Pages;
+using Project.ViewModel;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+namespace Project
+{
+    /// <summary>
+    /// Interaction logic for MainWindow.xaml
+    /// </summary>
+    public partial class MainWindow : Window
+    {
+        int ok = 0;
+        private FoodPage FoodPage = new FoodPage();
+        private AccountPage AccountPage = new AccountPage();
+        private RecommendPage RecommendPage = new RecommendPage();
+        private CalorieBurnPage CalorieBurnPage = new CalorieBurnPage();
+        public int UserID { get; set; }
+        public MainWindow()
+        {
+            InitializeComponent();
+            Main.Content = FoodPage;
+            ok = 1;
+        }
+
+        private void CalorieBurn_Checked(object sender, RoutedEventArgs e)
+        {
+            Main.Content = CalorieBurnPage;
+        }
+
+        private void Food_Checked(object sender, RoutedEventArgs e)
+        {
+            if (ok == 1) Main.Content = FoodPage;
+        }
+
+        private void Recommend_Checked(object sender, RoutedEventArgs e)
+        {
+            Main.Content = RecommendPage;
+        }
+
+        private void Account_Checked(object sender, RoutedEventArgs e)
+        {
+            Main.Content = AccountPage;
+        }
+        private void Minize_Click(object sender, RoutedEventArgs e)
+        {
+            if (this.WindowState != WindowState.Minimized)
+            {
+                this.WindowState = (WindowState.Minimized);
+            }
+            else { this.WindowState = WindowState.Normal; }
+        }
+        private void Maximize_Click(object sender, RoutedEventArgs e)
+        {
+            if (this.WindowState != WindowState.Maximized)
+            {
+                this.WindowState = (WindowState.Maximized);
+            }
+            else { this.WindowState = WindowState.Normal; }
+        }
+
+        private void Close_Click(object sender, RoutedEventArgs e)
+        {
+            this.Close();
+        }
+
+        private void window_MouseDown(object sender, MouseButtonEventArgs e)
+        {
+            if (e.LeftButton == MouseButtonState.Pressed)
+            {
+                DragMove();
+            }
+        }
+
+        private void Window_Loaded(object sender, RoutedEventArgs e)
+        {
+            //this.Hide();
+            LoginWindow loginWindow = new LoginWindow();
+            loginWindow.ShowDialog();
+            /*if (loginWindow.IsLogin)
+            {
+                this.Show();
+            }*/
+        }
+    }
+}
---
+++ Project/Project - packup/Pages/RecommendPage.xaml.cs
@@ -0,0 +1,106 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+namespace Project.Pages
+{
+    /// <summary>
+    /// Interaction logic for RecommendPage.xaml
+    /// </summary>
+    public partial class RecommendPage : Page
+    {
+        public RecommendPage()
+        {
+            InitializeComponent();
+            this.DataContext = this;
+            List<User> items = new List<User>();
+
+            items.Add(new User() { Name = "John Doe", Age = 42, Mail = "john@doe-family.com" });
+            items.Add(new User() { Name = "Jane Doe", Age = 39, Mail = "jane@doe-family.com" });
+            items.Add(new User() { Name = "Sammy Doe", Age = 13, Mail = "sammy.doe@gmail.com" });
+            items.Add(new User() { Name = "Sammy Doe", Age = 13, Mail = "sammy.doe@gmail.com" });
+            items.Add(new User() { Name = "Sammy Doe", Age = 13, Mail = "sammy.doe@gmail.com" });
+            items.Add(new User() { Name = "Sammy Doe", Age = 13, Mail = "sammy.doe@gmail.com" });
+            items.Add(new User() { Name = "Sammy Doe", Age = 13, Mail = "sammy.doe@gmail.com" });
+            items.Add(new User() { Name = "Sammy Doe", Age = 13, Mail = "sammy.doe@gmail.com" });
+            items.Add(new User() { Name = "Sammy Doe", Age = 13, Mail = "sammy.doe@gmail.com" });
+            items.Add(new User() { Name = "Sammy Doe", Age = 13, Mail = "sammy.doe@gmail.com" });
+            lvBreakfastRecommendation.ItemsSource = items;
+            lvLunchRecommendation.ItemsSource = items;
+            lvDinnerRecommendation.ItemsSource = items;
+        }
+        public class User
+        {
+            public string Name { get; set; }
+
+            public int Age { get; set; }
+
+            public string Mail { get; set; }
+        }
+        private void lvBreakfastRecommendation_SelectionChanged(object sender, SelectionChangedEventArgs e)
+        {
+            BitmapImage bitimg = new BitmapImage();
+            bitimg.BeginInit();
+            bitimg.UriSource = new Uri(@"\Assets\meat.png", UriKind.Relative);
+            bitimg.EndInit();
+            food_image.Source = bitimg;
+            food_review.Text = "This is a piece of meat!!!";
+        }
+        private void lvLunchRecommendation_SelectionChanged(object sender, SelectionChangedEventArgs e)
+        {
+            BitmapImage bitimg = new BitmapImage();
+            bitimg.BeginInit();
+            bitimg.UriSource = new Uri(@"\Assets\noodle.png", UriKind.Relative);
+            bitimg.EndInit();
+            food_image.Source = bitimg;
+            food_review.Text = "This is a piece of noodle!!!";
+        }
+        private void lvDinnerRecommendation_SelectionChanged(object sender, SelectionChangedEventArgs e)
+        {
+            BitmapImage bitimg = new BitmapImage();
+            bitimg.BeginInit();
+            bitimg.UriSource = new Uri(@"\Assets\rice.png", UriKind.Relative);
+            bitimg.EndInit();
+            food_image.Source = bitimg;
+            food_review.Text = "This is a piece of rice!!!";
+        }
+
+        private void btn_them_Click(object sender, RoutedEventArgs e)
+        {
+            TabItem selected_tab = (TabItem)tab_control.SelectedItem;
+            switch (selected_tab.Name)
+            {
+                case "breakfast":
+                    {
+                        SelectedFood_lv.Items.Add(lvBreakfastRecommendation.SelectedItem);
+                        break;
+                    }
+                case "lunch":
+                    {
+                        SelectedFood_lv.Items.Add(lvLunchRecommendation.SelectedItem);
+                        break;
+                    }
+                case "dinner":
+                    {
+                        SelectedFood_lv.Items.Add(lvDinnerRecommendation.SelectedItem);
+                        break;
+                    }
+                default:
+                    {
+                        return;
+                    }
+            }
+        }
+    }
+}
