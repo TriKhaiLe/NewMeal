3ca184a30000096822140cb0058de511746337d2
leehungw <hungttalop83@gmail.com>
2023-01-09 20:44:13 +0700

    Tabitem included in 1 listview

--- Project/Project/Pages/RecommendPage.xaml.cs
+++ Project/Project/Pages/RecommendPage.xaml.cs
@@ -1,7 +1,9 @@
 ﻿using Project.Model;
 using System;
 using System.Collections.Generic;
+using System.ComponentModel;
 using System.Linq;
+using System.Runtime.CompilerServices;
 using System.Text;
 using System.Threading.Tasks;
 using System.Windows;
@@ -20,240 +22,110 @@ namespace Project.Pages
     /// <summary>
     /// Interaction logic for RecommendPage.xaml
     /// </summary>
-    public partial class RecommendPage : Page
+    public partial class RecommendPage : Page , INotifyPropertyChanged
     {
         public List<UserFood> FoodUser { get; set; }
-        public List<Food> Breakfast_food { get; set; }
-        public List<Food> Lunch_food { get; set; }
-        public List<Food> Dinner_food { get; set; }
-        public List<Food> foodList { get; set; }
+        public List<FoodDays> Breakfast_food { get; set; }
+        public List<FoodDays> Lunch_food { get; set; }
+        public List<FoodDays> Dinner_food { get; set; }
+        private List<FoodDays> _foodList;
+        private int IsLoadFood;
+        public List<FoodDays> foodList
+        {
+            get
+            {
+                if (_foodList == null) _foodList = new List<FoodDays>();
+                return _foodList;
+            }
+            set
+            {
+                _foodList = value;
+                OnPropertyChanged();
+            }
+        }
+        public event PropertyChangedEventHandler PropertyChanged;
+
+        protected virtual void OnPropertyChanged([CallerMemberName] string propertyName = null)
+        {
+            PropertyChanged?.Invoke(this, new PropertyChangedEventArgs(propertyName));
+        }
         CollectionView view;
         public RecommendPage()
         {
             this.DataContext = this;
-            foodList = new List<Food>();
-            Breakfast_food = new List<Food>();
-            Lunch_food = new List<Food>();
-            Dinner_food = new List<Food>();
-            foodList = DataProvider.Ins.DB.Food.ToList();
-            FoodUser = DataProvider.Ins.DB.UserFood.Where(p => p.UserID == DataProvider.Ins.Current_UserID).ToList();
+            Breakfast_food = new List<FoodDays>();
+            Lunch_food = new List<FoodDays>();
+            Dinner_food = new List<FoodDays>();
+            IsLoadFood = 1;
             InitializeComponent();
-            view = (CollectionView)CollectionViewSource.GetDefaultView(lvBreakfastRecommendation.ItemsSource);
         }
 
         private void recommend_page_Loaded(object sender, RoutedEventArgs e)
         {
-            foreach (Food food in foodList)
+            FoodUser = DataProvider.Ins.DB.UserFood.Where(p => p.UserID == DataProvider.Ins.Current_UserID).ToList();
+            Food food = new Food();
+            List<Food> foods = DataProvider.Ins.DB.Food.ToList();
+            foreach (UserFood user in FoodUser)
             {
+                food = DataProvider.Ins.DB.Food.SingleOrDefault(p => p.FoodID == user.FoodID);
                 switch (food.MealTime)
                 {
                     case 3:
                         {
-                            Breakfast_food.Add(food);
+                            Breakfast_food.Add(new FoodDays(food, user.Last_eat, user.Favorite));
                         }
                         break;
                     case 4:
                         {
-                            Lunch_food.Add(food);
+                            Lunch_food.Add(new FoodDays(food, user.Last_eat, user.Favorite));
                         }
                         break;
                     case 5:
                         {
-                            Dinner_food.Add(food);
+                            Dinner_food.Add(new FoodDays(food, user.Last_eat, user.Favorite));
                         }
                         break;
                     case 7:
                         {
-                            Breakfast_food.Add(food);
-                            Lunch_food.Add(food);
+                            Breakfast_food.Add(new FoodDays(food, user.Last_eat, user.Favorite));
+                            Lunch_food.Add(new FoodDays(food, user.Last_eat, user.Favorite));
                         }
                         break;
                     case 8:
                         {
-                            Breakfast_food.Add(food);
-                            Dinner_food.Add(food);
+                            Breakfast_food.Add(new FoodDays(food, user.Last_eat, user.Favorite));
+                            Dinner_food.Add(new FoodDays(food, user.Last_eat, user.Favorite));
                         }
                         break;
                     case 9:
                         {
-                            Lunch_food.Add(food);
-                            Dinner_food.Add(food);
+                            Lunch_food.Add(new FoodDays(food, user.Last_eat, user.Favorite));
+                            Dinner_food.Add(new FoodDays(food, user.Last_eat, user.Favorite));
                         }
                         break;
                     case 12:
                         {
-                            Breakfast_food.Add(food);
-                            Lunch_food.Add(food);
-                            Dinner_food.Add(food);
+                            Breakfast_food.Add(new FoodDays(food, user.Last_eat, user.Favorite));
+                            Lunch_food.Add(new FoodDays(food, user.Last_eat, user.Favorite));
+                            Dinner_food.Add(new FoodDays(food, user.Last_eat, user.Favorite));
                         }
                         break;
                 }
-                lvBreakfastRecommendation.ItemsSource = Breakfast_food;
-                lvLunchRecommendation.ItemsSource = Lunch_food;
-                lvDinnerRecommendation.ItemsSource = Dinner_food;
+
             }
-            lvBreakfastRecommendation.Items.Refresh();
-            lvLunchRecommendation.Items.Refresh();
-            lvDinnerRecommendation.Items.Refresh();
+            if (IsLoadFood-- == 1)
+            {
+                tab_control.SelectedIndex = 0;
+            }
             Gauge_Kcal.To = DataProvider.Ins.Kcal_UserID;
         }
 
-        private void lvBreakfastRecommendation_SelectionChanged(object sender, SelectionChangedEventArgs e)
-        {
-            BitmapImage bitimg = new BitmapImage();
-            Food food = lvBreakfastRecommendation.SelectedItem as Food;
-            bitimg.BeginInit();
-            bitimg.UriSource = new Uri(food.Imgsrc, UriKind.Relative);
-            bitimg.EndInit();
-            food_image.Source = bitimg;
-            food_review.Text = food.Descript;
-            UserFood uf = DataProvider.Ins.DB.UserFood.SingleOrDefault(p => p.UserID == DataProvider.Ins.Current_UserID && p.FoodID == food.FoodID);
-            tb_calo.Text = food.Kcal.Value.ToString();
-            if (uf == null)
-            {
-                favorite_button.IsChecked = false;
-                tb_last_eat.Text = "Bạn chưa bao giờ thử món này!";
-            }
-            else
-            {
-                if (uf.Favorite == null)
-                {
-                    favorite_button.IsChecked = false;
-                }
-                else
-                {
-                    if (uf.Favorite == 1)
-                    {
-                        favorite_button.IsChecked = true;
-                    }
-                    else
-                    {
-                        favorite_button.IsChecked = false;
-                    }
-                }
-                if (uf.Last_eat == null)
-                {
-                    tb_last_eat.Text = "Bạn chưa bao giờ thử món này!";
-                }
-                else
-                {
-                    tb_last_eat.Text = uf.Last_eat.Value.ToShortDateString();
-                }
-            }
-        }
-        private void lvLunchRecommendation_SelectionChanged(object sender, SelectionChangedEventArgs e)
-        {
-            BitmapImage bitimg = new BitmapImage();
-            Food food = lvLunchRecommendation.SelectedItem as Food;
-            bitimg.BeginInit();
-            bitimg.UriSource = new Uri(food.Imgsrc, UriKind.Relative);
-            bitimg.EndInit();
-            food_image.Source = bitimg;
-            food_review.Text = food.Descript;
-            UserFood uf = DataProvider.Ins.DB.UserFood.SingleOrDefault(p => p.UserID == DataProvider.Ins.Current_UserID && p.FoodID == food.FoodID);
-            tb_calo.Text = food.Kcal.Value.ToString();
-            if (uf == null)
-            {
-                favorite_button.IsChecked = false;
-                tb_last_eat.Text = "Bạn chưa bao giờ thử món này!";
-            }
-            else
-            {
-                if (uf.Favorite == null)
-                {
-                    favorite_button.IsChecked = false;
-                }
-                else
-                {
-                    if (uf.Favorite == 1)
-                    {
-                        favorite_button.IsChecked = true;
-                    }
-                    else
-                    {
-                        favorite_button.IsChecked = false;
-                    }
-                }
-                if (uf.Last_eat == null)
-                {
-                    tb_last_eat.Text = "Bạn chưa bao giờ thử món này!";
-                }
-                else
-                {
-                    tb_last_eat.Text = uf.Last_eat.Value.ToShortDateString();
-                }
-            }
-        }
-        private void lvDinnerRecommendation_SelectionChanged(object sender, SelectionChangedEventArgs e)
-        {
-            BitmapImage bitimg = new BitmapImage();
-            Food food = lvDinnerRecommendation.SelectedItem as Food;
-            bitimg.BeginInit();
-            bitimg.UriSource = new Uri(food.Imgsrc, UriKind.Relative);
-            bitimg.EndInit();
-            food_image.Source = bitimg;
-            food_review.Text = food.Descript;
-            UserFood uf = DataProvider.Ins.DB.UserFood.SingleOrDefault(p => p.UserID == DataProvider.Ins.Current_UserID && p.FoodID == food.FoodID);
-            tb_calo.Text = food.Kcal.Value.ToString();
-            if (uf == null)
-            {
-                favorite_button.IsChecked = false;
-                tb_last_eat.Text = "Bạn chưa bao giờ thử món này!";
-            }
-            else
-            {
-                if (uf.Favorite == null)
-                {
-                    favorite_button.IsChecked = false;
-                }
-                else
-                {
-                    if (uf.Favorite == 1)
-                    {
-                        favorite_button.IsChecked = true;
-                    }
-                    else
-                    {
-                        favorite_button.IsChecked = false;
-                    }
-                }
-                if (uf.Last_eat == null)
-                {
-                    tb_last_eat.Text = "Bạn chưa bao giờ thử món này!";
-                }
-                else
-                {
-                    tb_last_eat.Text = uf.Last_eat.Value.ToShortDateString();
-                }
-            }
-        }
 
         private void btn_them_Click(object sender, RoutedEventArgs e)
         {
             TabItem selected_tab = (TabItem)tab_control.SelectedItem;
             Food food = new Food();
-            switch (selected_tab.Name)
-            {
-                case "breakfast":
-                    {
-                        food = (Food)lvBreakfastRecommendation.SelectedItem;
-                        break;
-                    }
-                case "lunch":
-                    {
-                        food = (Food)lvLunchRecommendation.SelectedItem;
-                        break;
-                    }
-                case "dinner":
-                    {
-                        food = (Food)lvDinnerRecommendation.SelectedItem;
-                        break;
-                    }
-                default:
-                    {
-                        return;
-                    }
-            }
+            food = (Food)lvRecommendation.SelectedItem;
             if (food != null)
             {
                 Gauge_Kcal.Value += (double)food.Kcal;
@@ -279,40 +151,93 @@ namespace Project.Pages
         private void favorite_button_Click(object sender, RoutedEventArgs e)
         {
             Food food = new Food();
-            switch (tab_control.SelectedIndex)
+            food = lvRecommendation.SelectedItem as Food;
+            UserFood userFood = DataProvider.Ins.DB.UserFood.SingleOrDefault(p => p.UserID == DataProvider.Ins.Current_UserID && p.FoodID == food.FoodID);
+            userFood.Favorite = Convert.ToInt16(favorite_button.IsChecked);
+            DataProvider.Ins.DB.SaveChanges();
+        }
+
+        private void ComboBox_SelectionChanged(object sender, SelectionChangedEventArgs e)
+        {
+            ComboBox cb = sender as ComboBox;
+
+            view = (CollectionView)CollectionViewSource.GetDefaultView(SelectedFood_lv.Items);
+
+            if (cb.SelectedIndex == 0)
             {
-                case 0:
-                    {
-                        food = lvBreakfastRecommendation.SelectedItem as Food;
-                    }
-                    break;
-                case 1:
-                    {
-                        food = lvLunchRecommendation.SelectedItem as Food;
-                    }
-                    break;
-                case 2:
-                    {
-                        food = lvDinnerRecommendation.SelectedItem as Food;
-                    }
-                    break;
 
             }
-            UserFood userFood = DataProvider.Ins.DB.UserFood.SingleOrDefault(p => p.UserID == DataProvider.Ins.Current_UserID && p.FoodID == food.FoodID);
-            if (userFood == null)
+            else if (cb.SelectedIndex == 1)
             {
-                userFood = new UserFood();
-                userFood.UserID = DataProvider.Ins.Current_UserID;
-                userFood.FoodID = food.FoodID;
-                userFood.Favorite = Convert.ToInt16(favorite_button.IsChecked);
-                userFood.Last_eat = null;
-                DataProvider.Ins.DB.UserFood.Add(userFood);
-                DataProvider.Ins.DB.SaveChanges();
+
+            }
+            else if (cb.SelectedIndex == 2)
+            {
+
+            }
+        }
+
+        private void tab_control_SelectionChanged(object sender, SelectionChangedEventArgs e)
+        {
+            if (tab_control.SelectedIndex == 0)
+            {
+                foodList = Breakfast_food;
+            }
+            else if (tab_control.SelectedIndex == 1)
+            {
+                foodList = Lunch_food;
+            }
+            else if (tab_control.SelectedIndex == 2)
+            {
+                foodList = Dinner_food;
+            }
+        }
+
+        private void lvRecommendation_SelectionChanged(object sender, SelectionChangedEventArgs e)
+        {
+            FoodDays food = ((ListView)sender).SelectedItem as FoodDays;
+            if (food == null)
+            {
+                food = foodList.First<FoodDays>();
+            }
+            BitmapImage bitimg = new BitmapImage();
+            bitimg.BeginInit();
+            bitimg.UriSource = new Uri(food.Food.Imgsrc, UriKind.Relative);
+            bitimg.EndInit();
+            food_image.Source = bitimg;
+            food_review.Text = food.Food.Descript;
+            UserFood uf = DataProvider.Ins.DB.UserFood.SingleOrDefault(p => p.UserID == DataProvider.Ins.Current_UserID && p.FoodID == food.Food.FoodID);
+            tb_calo.Text = food.Food.Kcal.Value.ToString();
+            if (uf == null)
+            {
+                favorite_button.IsChecked = false;
+                tb_last_eat.Text = "Bạn chưa bao giờ thử món này!";
             }
             else
             {
-                userFood.Favorite = Convert.ToInt16(favorite_button.IsChecked);
-                DataProvider.Ins.DB.SaveChanges();
+                if (uf.Favorite == null)
+                {
+                    favorite_button.IsChecked = false;
+                }
+                else
+                {
+                    if (uf.Favorite == 1)
+                    {
+                        favorite_button.IsChecked = true;
+                    }
+                    else
+                    {
+                        favorite_button.IsChecked = false;
+                    }
+                }
+                if (uf.Last_eat == null)
+                {
+                    tb_last_eat.Text = "Bạn chưa bao giờ thử món này!";
+                }
+                else
+                {
+                    tb_last_eat.Text = uf.Last_eat.Value.ToShortDateString();
+                }
             }
         }
     }
